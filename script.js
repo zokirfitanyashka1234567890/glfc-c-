9fba0c473efe2c0ff85c7cc9ef0ac48d680ed819
zokirfitanyashka1234567890 <n6209061@gmail.com>
2025-03-11 11:05:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('ZOkir');
+alert('zokir ukam')
+
+var name = "Alex"
+console.log(name)
+
+let age = 25;
+age = 30;
+console.log(age);
+
+const pi = 3.14;
+pi = 3.1415;
+
+let name1 = prompt()
+console.log(name1)
+
+let name = prompt("What is your name?")
+let familya = prompt("What is your surname?")
+let vozrast = prompt("How old are you?")
+let rabota = prompt("QAtta ishlaysiz?")
+alert(`Меня зовут ${name} ${familya}, мне ${vozrast} лет, я работаю ${rabota}`)
